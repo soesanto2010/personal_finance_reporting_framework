9265e26cdd21dd7347a020442465c0d5cbe3721b
vsoesanto <soesanto@wisc.edu>
2023-05-07 20:57:09 -0400

    Enable the closing / end date of the transactions to be determined either based on (1) a default (for runs ending in past dates, MBR etc.) or the current date (for the standard weekly closing, WBR etc.)

--- main_script.py
+++ main_script.py
@@ -15,23 +15,36 @@ from balance_sheet_calculations import get_account_level_balance_sheet, get_acco
 from income_statement_calculations import get_revenue_trend, get_expenses_trend, calculate_financial_KPIs
 from run_qc_tests import verify_accounting_equation, ensure_closing_date_is_later_than_initiation_dates, ensure_no_non_standard_account_names
 from output_generation import generate_csv_outputs
+import pandas as pd
 
 ##############################################################################################################
 
 # Part 2: Set run parameters
 
-Start_Date = datetime.date(2021,6,25)        # start of date for tracking expenses
-End_Date = datetime.date(2023,5,5)
+default_start_date = '2021-06-25 00:00:00' # start of date for tracking expenses
+default_closing_date = '2023-05-05 18:00:00' # (enter 'YYYY-MM-DD HH:MM:SS' to close on a specific (past) date, otherwise enter None to close on the current datetime)
+default_timezone = 'UTC'
 path = 'C:\\Users\\feiya\\OneDrive\\Desktop\\Financial Management\\'
 filename = 'Data Structure.xlsx'
 
 ##############################################################################################################
 
-# Part 3: Main script
+# Part 3: Determine the start & end dates for the transactions data
+
+Start_Date = pd.Timestamp(default_start_date, tz=default_timezone)
+
+if default_closing_date is None:
+    End_Date = pd.Timestamp(pd.Timestamp.now(), tz=default_timezone)
+else:
+    End_Date = pd.Timestamp(default_closing_date, tz=default_timezone)
+
+##############################################################################################################
+
+# Part 4: Main script
 
 def main(path,filename,Start_Date,End_Date):
 
-    # (1) Initialization & cleaning
+    # (a) Initialization & cleaning
     data_path = path
     End_Date = End_Date
     Start_Date = Start_Date
@@ -43,24 +56,24 @@ def main(path,filename,Start_Date,End_Date):
         end_date=End_Date)
     datasets.preprocess_transactions()
 
-    # (2) Get balance sheet level items
+    # (b) Get balance sheet level items
     get_account_level_balance_sheet(datasets)
     get_account_type_level_balance_sheet(datasets)
     get_overall_balance_sheet(datasets)
 
-    # (3) Get revenue and expense trends
+    # (c) Get revenue and expense trends
     get_revenue_trend(datasets)
     get_expenses_trend(datasets)
 
-    # (4) Ratio calculations
+    # (d) Ratio calculations
     calculate_financial_KPIs(datasets)
 
-    # (5) Run QC tests
+    # (e) Run QC tests
     verify_accounting_equation(datasets)
     ensure_closing_date_is_later_than_initiation_dates(datasets)
     ensure_no_non_standard_account_names(datasets)
 
-    # (6) Generate output files
+    # (f) Generate output files
     generate_csv_outputs(datasets)
 
     print("run completed")
