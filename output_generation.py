19f8c37a460f8d4e4824ced5a6820db46e5c3fdb
vsoesanto <soesanto@wisc.edu>
2023-05-30 20:24:37 -0400

    Removing module for calculating financial KPIs (will be handled via Google Data Studio based on the outputs in BigQuery)

--- output_generation.py
+++ output_generation.py
@@ -14,9 +14,7 @@ def generate_csv_outputs(self):
     self.Acct_Level_Summary.to_excel(writer, sheet_name="Account")
     self.P_L_statement_overall.to_excel(writer, sheet_name="monthly_P_L_overall")
     self.P_L_statement_deep_dive.to_excel(writer, sheet_name="monthly_P_L_deep_dive")
-    self.Summary_KPI.to_excel(writer, sheet_name="Summary_KPI")
     self.securities.to_excel(writer, sheet_name="Security_valuations")
-    self.Transactions.to_excel(writer, sheet_name='transactions')
 
     # Close the Pandas Excel writer and output the Excel file.
     writer.save()
